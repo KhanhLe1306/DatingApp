c5f8be182b864a9fe452f6659a062f09dd84e338
Khanh Le <khanhle@unomaha.edu>
2022-12-19 21:04:04 -0600

    Set up API

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var app = builder.Build();
+
+
+app.MapControllers();
+
+app.Run();
